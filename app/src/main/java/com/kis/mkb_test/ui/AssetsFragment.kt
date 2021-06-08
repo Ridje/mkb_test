adb199cb193505ff2cdd3076944e0cccdb3642ee
ridje <angelnumber59@gmail.com>
2021-06-08 12:33:36 +0300

    added dependencies

---
+++ app/src/main/java/com/kis/mkb_test/ui/AssetsFragment.kt
@@ -0,0 +1,30 @@
+package com.kis.mkb_test.ui
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kis.mkb_test.R
+
+class AssetsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AssetsFragment()
+    }
+
+    private lateinit var viewModel: AssetsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.assets_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(AssetsViewModel::class.java)
+    }
+}
---
+++ app/src/main/java/com/kis/mkb_test/ui/AssetsViewModel.kt
@@ -0,0 +1,7 @@
+package com.kis.mkb_test.ui
+
+import androidx.lifecycle.ViewModel
+
+class AssetsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
